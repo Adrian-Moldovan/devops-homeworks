3a1afd8590d4597236636c2c1e38d20c2344f908
Adrian Moldovan <ma.moldovan@gmail.com>
2026-08-21 09:16:45 +0300

    minor fixes and code grooming after code review

--- bash_assignments/setup_project.sh
+++ bash_assignments/setup_project.sh
@@ -4,8 +4,8 @@ _GREEN="\033[32m"
 _RED="\033[31m"
 _RESET="\033[0m"
 
-_SUCCESS_ICON="$_GREEN ✓ $_RESET"
-_ERROR_ICON="$_RED ✓ $_RESET"
+_SUCCESS_ICON="[$_GREEN✓$_RESET]"
+_ERROR_ICON="[$_RED✗$_RESET]"
 
 printf "\n======================\n"
 echo "Setup project started."
@@ -14,19 +14,21 @@ printf "======================\n\n"
 for ((i=1; i<=5; i++)); do
     # create directory
     dir_name="module_$i" 
-    if mkdir "module_$i"; then
-        printf "${_SUCCESS_ICON} Directory: '$dir_name' created...\n"
+    if mkdir "$dir_name"; then
+        printf "%b Directory: '%s' created...\n" "$_SUCCESS_ICON" "$dir_name"
     else 
-        printf "${_ERROR_ICON} Error creating directory: $dir_name.\n"
+        printf "%b Error creating directory: '%s'.\n" "$_ERROR_ICON" "$dir_name"
+        echo "Exiting script..."
         exit 1
     fi
 
     # create file
-    file="module_$i/notes.md"
+    file="$dir_name/notes.md"
     if touch "$file"; then
         printf "${_SUCCESS_ICON} File: $file created...\n\n"
     else
         printf "${_ERROR_ICON} Error creating file: $file"
+        echo "Exiting script..."
         exit 1
     fi
 done
